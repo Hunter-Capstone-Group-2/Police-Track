ecccf7f71034c5c1730fd2bd49834ad25dd8d9dd
Ramy Hammam <56901837+ramy523@users.noreply.github.com>
2023-02-23 18:53:42 -0500

    Add files via upload

---
+++ copwatch/copwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  copwatchApp.swift
+//  copwatch
+//
+//  Created by Ramy on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct copwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
