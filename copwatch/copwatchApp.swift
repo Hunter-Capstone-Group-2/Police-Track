74a510e0c442ff571aa263ae362e0289e1e18aec
Ramy Hammam <56901837+ramy523@users.noreply.github.com>
2023-02-25 17:11:33 -0500

    Add files via upload
    initial commit

---
+++ copwatch/copwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  copwatchApp.swift
+//  copwatch
+//
+//  Created by Ramy on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct copwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ copwatch/views/Signupview.swift
@@ -0,0 +1,109 @@
+//
+//  Signupview.swift
+//  copwatch
+//
+//  Created by Ramy on 2/23/23.
+//
+import SwiftUI
+
+struct Signupview: View {
+    @Binding var currentShowingView: String
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+        ZStack{
+            Color.black .edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Text("Create An Account")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .multilineTextAlignment(.center)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    
+                    
+                    Spacer()
+                }
+                .padding()
+                .padding(.top)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "mail")
+                    TextField("Email / Username", text: $email)
+                    
+                    Spacer()
+                    
+                }
+                .foregroundColor(.white)
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.white)
+                }
+                
+                .padding()
+                
+                
+                HStack {
+                    Image(systemName: "lock")
+                    SecureField("Password", text: $password)
+                    
+                    Spacer()
+                    
+                }
+                .foregroundColor(.white)
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.white)
+                }
+                
+                .padding()
+                
+                Button(action: {
+                    withAnimation {
+                        self.currentShowingView = "login"
+                    }
+                    
+                }) {
+                    Text("Already have an account? ")
+                        .foregroundColor(.gray)
+                    
+                }
+                
+                
+                
+                
+                Spacer()
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Register ")
+                        .foregroundColor(.black)
+                        .font(.title3)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    
+                        .background(
+                            RoundedRectangle(cornerRadius: 100)
+                                .fill(Color.white)
+                        )
+                        .padding(.horizontal )
+                    
+                }
+                
+                
+            }
+            
+        }
+    }
+}
