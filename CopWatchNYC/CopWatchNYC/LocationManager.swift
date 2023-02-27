373fcbe26c90991acfd0c86bcfdbd7e442b5efa1
Alex <DiaA6383@gmail.com>
2023-02-27 12:22:59 -0500

    cleaned up LocationManaager

--- CopWatchNYC/CopWatchNYC/LocationManager.swift
+++ CopWatchNYC/CopWatchNYC/LocationManager.swift
@@ -3,12 +3,10 @@ import CoreLocation
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
-    
     @Published var location: CLLocation?
     
     override init() {
         super.init()
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
